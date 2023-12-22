8a2245cd27174fb77df1ba76113585cafd82e427
NavidNikoo <121324067+NavidNikoo@users.noreply.github.com>
2023-12-21 17:24:13 -0800

    Update App.java

--- App.java
+++ App.java
@@ -1,9 +1,6 @@
-
-// Importing utility classes for handling collections (like ArrayList) and date formatting.
 import java.util.*;
 import java.text.*;
 
-// Defining an interface for savings accounts with constants and abstract methods.
 interface SavingsAccount {
     // Constants for interest rate and account limits.
     final double rate = 0.04, limit = 10000, limit1 = 200;
@@ -13,29 +10,27 @@ interface SavingsAccount {
     void withdraw(double n, Date d);
 }
 
-// Customer class implementing the SavingsAccount interface.
 class Customer implements SavingsAccount {
-    // Customer attributes: username, password, personal details, and account balance.
+
     String username, password, name, address, phone;
     double balance;
+
     // List to store transaction history.
     ArrayList<String> transactions;
 
-    // Constructor for Customer class.
     Customer(String username, String password, String name, String address, String phone, double balance, Date date) {
-        // Initializing customer attributes with provided values.
         this.username = username;
         this.password = password;
         this.name = name;
         this.address = address;
         this.phone = phone;
         this.balance = balance;
+
         // Initializing the transactions list and adding the initial deposit transaction.
         transactions = new ArrayList<String>(5);
         addTransaction(String.format("Initial deposit - " + NumberFormat.getCurrencyInstance().format(balance) + " as on " + "%1$tD" + " at " + "%1$tT.", date));
     }
 
-    // Method to update the account balance based on certain conditions.
     void update(Date date) {
         // If balance is greater than or equal to the limit, apply interest.
         if (balance >= 10000) {
@@ -44,11 +39,11 @@ class Customer implements SavingsAccount {
             // If balance is less than the limit, apply a penalty.
             balance -= (int) (balance / 100.0);
         }
+        
         // Record the account update transaction.
         addTransaction(String.format("Account updated. Balance - " + NumberFormat.getCurrencyInstance().format(balance) + " as on " + "%1$tD" + " at " + "%1$tT.", date));
     }
 
-    // Overriding the deposit method from the SavingsAccount interface.
     @Override
     public void deposit(double amount, Date date) {
         // Increasing the account balance by the deposit amount.
@@ -57,18 +52,19 @@ class Customer implements SavingsAccount {
         addTransaction(String.format(NumberFormat.getCurrencyInstance().format(amount) + " credited to your account. Balance - " + NumberFormat.getCurrencyInstance().format(balance) + " as on " + "%1$tD" + " at " + "%1$tT.", date));
     }
 
-     // Overriding the withdraw method from the SavingsAccount interface.
+     
     @Override
     public void withdraw(double amount, Date date) {
-        // Check if the withdrawal amount is more than the available balance minus the minimum required balance (200).
-        if (amount > (balance - 200)) {
-            // If the balance is insufficient for the withdrawal, print a message and exit the method.
-            System.out.println("Insufficient balance.");
+
+        if (amount > (balance - 200)) { //if, the withdrawal amount is more than the available balance minus the minimum required balance (200)
+            System.out.println("Insufficient balance!"); // then, the balance is insufficient for the withdrawal, print a message and exit the method
             return;
         }
-        // Deduct the amount from the balance.
+
+        //else, deduct the amount from the balance.
         balance -= amount;
-        // Record the withdrawal transaction with formatted date and time.
+        
+        // Record the withdrawal transact ion with formatted date and time.
         addTransaction(String.format(NumberFormat.getCurrencyInstance().format(amount) + " debited from your account. Balance - " + NumberFormat.getCurrencyInstance().format(balance) + " as on " + "%1$tD" + " at " + "%1$tT.", date));
     }
 
@@ -95,14 +91,15 @@ class Bank {
         customerMap = new HashMap<String, Customer>();
     }
 
-    // Main method - the entry point of the program
-    public static void main(String[] args) {
-        // Scanner object to read input from the console
+    
+    public static void main(String[] args) { // Main method 
+
+        
         Scanner sc = new Scanner(System.in);
-        // Variables to store customer details and transaction amount
         Customer customer;
         String username, password;
         double amount;
+
         // Creating a new instance of Bank
         Bank bank = new Bank();
         // Variable to store the user's menu choice
@@ -178,11 +175,11 @@ class Bank {
                    // Retrieving the customer object associated with the username
                    customer = bank.customerMap.get(username);
 
-               // Checking if the entered password matches the customer's password
-                   if (customer.password.equals(password)) {
-                // Entering a sub-menu for the logged-in user
+               
+                   if (customer.password.equals(password)){//if, the entered password matches the customer's password
+                //Sub-Menu
                 while (true) {
-                // Displaying options for the logged-in user
+                //Displaying options for the logged-in user
                 System.out.println("\n-------------------");
                 System.out.println("W E L C O M E");
                 System.out.println("-------------------\n");
@@ -195,10 +192,10 @@ class Bank {
                 
                 // Reading the user's choice for the sub-menu
                 choice = sc.nextInt();
-							sc.nextLine(); // Consuming the leftover newline character
+				sc.nextLine(); //gets rid of leftover newline character
+
             switch(choice) {
-             case 1:
-             // Option for depositing money
+             case 1:// Option for depositing money
                 System.out.print("Enter amount: ");
                 // Validating that the entered amount is a double
 
@@ -208,7 +205,7 @@ class Bank {
         }
 
         amount = sc.nextDouble(); // Reading the valid deposit amount
-        sc.nextLine(); // Consuming the leftover newline character
+        sc.nextLine();
         customer.deposit(amount, new Date()); // Performing the deposit operation
         break;
 
@@ -232,6 +229,7 @@ class Bank {
             System.out.println("Transfer limit exceeded. Contact bank manager.");
             break; // Exiting the case if the limit is exceeded
         }
+
         // Checking if the payee's username exists in the bank's customer map
         if (bank.customerMap.containsKey(username)) {
             Customer payee = bank.customerMap.get(username); // Retrieving the payee's account
